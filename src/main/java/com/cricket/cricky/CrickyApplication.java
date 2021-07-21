900ba938b68114a5ac384dda83b04d7181820e0e
sushovan.karmakar <sushovan.karmakar@randstadrisesmart.com>
2021-07-21 22:05:04 +0530

    initial commit

---
+++ src/main/java/com/cricket/cricky/CrickyApplication.java
@@ -0,0 +1,13 @@
+package com.cricket.cricky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrickyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrickyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricket/cricky/CrickyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.cricky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrickyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
